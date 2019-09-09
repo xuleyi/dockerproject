c256bf112e13e083ff36fbdfe6123ed3c31cd9d1
xuleyi <xuleyitud@gmail.com>
2019-09-09 14:02:48 +0200

    lean_nfv_edit

--- lean_nfv/lean_nfv.sh
+++ lean_nfv/lean_nfv.sh
@@ -2,13 +2,14 @@
 #echo '' > t2_rp.txt
 
 sshpass -p "nuc" scp run1ln.sh nuc2@192.168.1.1:~/project
-sshpass -p "123456" scp run2ln.sh ann@192.168.1.2:~/project
 
+#sshpass -p "123456" ssh ann@192.168.1.2 "cd ~/project; docker run -d --name lntest-clone myal:testln"
 
 #for ((i=0:i<50;i++))
 #do
-sshpass -p "nuc" ssh nuc2@192.168.1.1 "cd ~/project; bash run1ln.sh" && sshpass -p "123456" ssh ann@192.168.1.2 "cd ~/project; bash run2ln.sh"
+sshpass -p "nuc" ssh nuc2@192.168.1.1 "cd ~/project; bash run1ln.sh" 
+#&& sshpass -p "123456" ssh ann@192.168.1.2 "cd ~/project; bash run2ln.sh"
 #done
 
-sleep 6
-sshpass -p "123456" sh ann@192.168.1.2 "cd ~/project; docker container stop lntest-clone;docker rm lntest-clone"
+#sleep 6
+#sshpass -p "123456" ssh ann@192.168.1.2 "cd ~/project; docker rm -f lntest-clone"
--- lean_nfv/run1ln.sh
+++ lean_nfv/run1ln.sh
@@ -1,14 +1,13 @@
-#sshpass -p vw4 ssh vw4@192.168.0.4 "cd ~/project; python lnclient_dc.py"
+docker run -d --name lntest myal:testln1 && sshpass -p vw4 ssh vw4@192.168.0.4 "cd ~/project; python lnclient_dc.py &" 
 
-docker run -d --name lntest myal:testdow
+wait
 echo "run lntest"
 sleep 2
 
 t1=$(date +%s.%N)
 echo $t1
 
-docker stop lntest 
-docker rm lntest
+docker rm -f lntest
 
 
 
